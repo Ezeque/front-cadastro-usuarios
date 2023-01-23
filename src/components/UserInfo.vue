558698bbc1cfb22dbedbcf5271b8a3ccc9a2dd84
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-01-22 22:23:32 -0300

    adicionado título na informação de usuário

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,9 +1,13 @@
 <template>
     <div id="pannel">
         <AvatarArea class="avatar-area" />
-        <div style="display:flex; flex-direction: column">
+        <div style="display:flex; flex-direction: column; flex-wrap: wrap;">
             <h3 class="user-name">{{ `${userDetails.firstName} ${userDetails.lastName}` }}</h3>
             <h4 class="user-email">{{ `${userDetails.email}` }}</h4>
+            <div style="text-align:left">
+                <span class="user-title">{{ userDetails.title }}</span>
+            </div>
+            
         </div>
     </div>
 </template>
@@ -13,6 +17,18 @@ import AvatarArea from '@/components/widgets/AvatarArea.vue'
 </script>
 
 <style>
+
+.user-title{
+    padding: 5px 10px;
+    color: white;
+    font-weight: bold;
+    font-style: italic;
+    background-color: #172940;
+    border-radius: 10px;
+    text-align: left;
+    width:auto !important;
+}
+
 #pannel {
     display: flex;
     align-items: center;
@@ -24,12 +40,14 @@ import AvatarArea from '@/components/widgets/AvatarArea.vue'
 .user-name {
     color: #172940;
     margin: 0;
+    margin-bottom: 10px;
     text-align: left;
 }
 
 .user-email{
     margin: 0;
     color: #a2b5cd;
+    margin-bottom: 15px;
     text-align: left;
 }
 </style>
